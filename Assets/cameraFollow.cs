4eeb3b0debb061e0788a54ff2d915a2912b39069
Cesar <ctmces94@gmail.com>
2019-06-30 21:52:07 -0500

    Assets y Script

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    private float xmin = 0;
+    private float ymin = 0;
+    private float xmax = -30;
+    private float ymax = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+        transform.position = player.transform.position + offset;
+        
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Rigidbody2D body;
+    public float walkingSpeed;
+    public float jumpSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { // x-axis movement
+            body.velocity += jumpSpeed * Vector2.up;
+        }
+
+        { // x-axis movement
+            var v = body.velocity;
+            var speed = 0f;
+            var flipLeft = 0;
+            
+            
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    speed += -walkingSpeed;
+                    flipLeft = 1;
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    speed += walkingSpeed;
+                    flipLeft = 0;
+                }
+            
+
+            if (flipLeft == 1)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            v.x = speed;
+            
+            body.velocity = v;
+        }
+
+    }
+}
